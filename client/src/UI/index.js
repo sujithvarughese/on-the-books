5def6ebef20b9cccb8765a299e23a3b265d25f02
sujithv15 <64935098+sujithv15@users.noreply.github.com>
2023-10-27 15:48:34 -0400

    styling- added card background for book component

--- client/src/UI/index.js
+++
@@ -1,9 +0,0 @@
-import Backdrop from "./Backdrop.jsx";
-import Button from "./Button.jsx";
-import Card from "./Card.jsx"
-import Form from "./Form.jsx";
-import FormSelect from "./FormSelect.jsx";
-import Input from "./Input.jsx";
-import Modal from "./Modal.jsx"
-
-export { Backdrop, Card, Input, Button, Form, Modal, FormSelect }
