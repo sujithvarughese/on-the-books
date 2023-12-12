746fa3584221ea156a06f3faa65774ad36e5f024
Sujith Varughese <sujithv15@gmail.com>
2023-12-12 01:28:11 -0500

    more styling

--- server/routes/notebook-router.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router()
-
-import { getNotebook, createNote, updateNote } from "../controllers/notebook-controller.js";
-
-router.route("/:id").get(getNotebook)
-router.route("/").post(createNote).patch(updateNote)
-
-export default router
